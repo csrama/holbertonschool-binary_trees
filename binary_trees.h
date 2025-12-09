cd2074dfddf97458867dff2b5e7ac0a1ec768be3
csrama <ramadafer1@gmail.com>
2025-12-09 08:05:56 +0000

    implement array_to_bst function

--- binary_trees.h
+++ binary_trees.h
@@ -65,6 +65,6 @@ int binary_tree_is_bst(const binary_tree_t *tree);
 
 /* BST - Insert */
 bst_t *bst_insert(bst_t **tree, int value);
-
+bst_t *array_to_bst(int *array, size_t size);
 #endif /* BINARY_TREES_H */
 
