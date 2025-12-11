8bb939811ea05a143bf8e4452aadec7b76f998ab
csrama <ramadafer1@gmail.com>
2025-12-11 06:46:52 +0000

    Find sibling of binary tree node (other child of same parent).

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -4,7 +4,7 @@
  * binary_tree_sibling - finds the sibling of a node
  * @node: pointer to node to find sibling
  *
- * Return: pointer to sibling node, or NULL if no sibling
+ * Return: pointer to sibling node, or NULL if no sibling.
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
