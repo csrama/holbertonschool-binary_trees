21ed572c2668630353f403807420a8478067b7f3
janabakri <bkeyh382@gmail.com>
2025-12-12 00:24:20 +0000

    Implement left rotation for binary trees, updating child and parent pointers to correctly restructure the tree and maintain proper hierarchy.

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 
-/**
+  /**
  * binary_tree_rotate_left - Performs a left rotation on a binary tree
  * @tree: Pointer to the root node of the tree to rotate
  * Return: Pointer to the new root node after rotation
