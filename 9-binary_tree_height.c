8f804af096725f59c70be7fd4fbd47859de0a353
jana <bkeyh382@gmail.com>
2025-12-14 11:13:28 +0300

    Correct punctuation in binary_tree_height documentation

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -4,7 +4,7 @@
  * binary_tree_height - Measures the height of a binary tree
  * @tree: Pointer to the root node of the tree to measure the height
  *
- * Return: Height of the tree, 0 if tree is NULL
+ * Return: Height of the tree, 0 if tree is NULL.
  */
 
 size_t binary_tree_height(const binary_tree_t *tree)
